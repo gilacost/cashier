0c2d66edf7c712cd3ad9d4d0e3d234d4786895fa
gilacost <josepgiraltdlacoste@gmail.com>
2020-01-18 08:58:43 +0100

    feat: adds product to checkout items list

--- lib/checkout.ex
+++ lib/checkout.ex
@@ -2,7 +2,7 @@ defmodule Checkout do
   @moduledoc """
   """
 
-  defstruct items: [], price_rules: []
+  defstruct items: %{}, price_rules: %{}
 
   @type t :: %__MODULE__{
           items: list,
@@ -11,4 +11,14 @@ defmodule Checkout do
 
   @spec new(list) :: t
   def new(price_rules), do: %Checkout{price_rules: price_rules}
+
+  @spec add_item(t, String.t()) :: t
+  def add_item(checkout, product) do
+    Map.update(
+      checkout,
+      :items,
+      [product],
+      fn products -> [product | products] end
+    )
+  end
 end
