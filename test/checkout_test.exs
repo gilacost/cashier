b02507caf671c8b1ac7482cdc11b0c0f3c80769a
gilacost <josepgiraltdlacoste@gmail.com>
2020-01-18 09:58:21 +0100

    test: mix test in pre-commit

--- test/checkout_test.exs
+++ test/checkout_test.exs
@@ -2,6 +2,8 @@ defmodule CheckoutTest do
   use ExUnit.Case
   alias Checkout.Product
 
+  @valid_product_codes ["GR1", "SR1", "CR1"]
+
   describe "checkout" do
     test "new" do
       assert %Checkout{items: [], price_rules: []} = Checkout.new([])
@@ -17,6 +19,17 @@ defmodule CheckoutTest do
 
   describe "product" do
     test "new has code, name and price" do
+      assert {:ok, %Product{code: _, name: _, price: _}} = Product.new("GR1")
+    end
+
+    test "valid products" do
+      # TODO get them from env?
+      # TODO setup all
+
+      Enum.each(@valid_product_codes, fn code ->
+        assert %Product{} = Product.new(code)
+      end)
+
       assert {:ok, %Product{code: _, name: _, price: _}} = Product.new("GR1")
     end
   end
