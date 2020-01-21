45f3c6419b9999c44f1a19e9e5b539f3d66692a7
gilacost <josepgiraltdlacoste@gmail.com>
2020-01-21 01:13:05 +0100

    parent d1a14864ea4b8a418066d060e17cf8c21177f7be author gilacost <josepgiraltdlacoste@gmail.com> 1579565585 +0100 committer gilacost <josepgiraltdlacoste@gmail.com> 1579625509 +0100 gpgsig -----BEGIN PGP SIGNATURE-----
     iQIzBAABCAAdFiEE4k1Q2qMhZ1y4B3Ctity1eLM4AZAFAl4nLCUACgkQity1eLM4
     AZCk8hAAiA4z5c73Z8o9y17rz6/IeSvLtLRMNHeQgdZCH4BmGXYbspIqz+HhzKkZ
     dTxnsd4AsMiAaHuEU0ssYRgthWjJOb7lPiOb2feDmCZX1eFJ6xnOlLuoiwCKjTMY
     Pl4CJh0VR+7scPsbHqFWADR3jcWrhCWWu011e1FXNVNj81gnFMZOgg6pYdY7sSys
     T9GI8FDVT6xYWX8jE0p9nRucr5sQONFwYRlaaa5gv1U/7NZuXZ3CDfqdeK66pilo
     3b0E8fGMJLt1R1qtd7mI3YM0c+1RtcpKH0DYo/sa5aQNFXvcNZdCK+7vmENqRx5/
     VnR+rjj+7AMbBC+i9ryrLHypB4tezySMUoMCdhivhR67VKbxVstqJmYjAP8x5PID
     ymdS57MsxmtZo5Lxqg/tnCmTBvNxO2VJlvx8Fww9AMwE8jr9a42xjEyu3KdyN2gQ
     NwAyWGHStTjYuW6atwcdZ5+z+pJWHzzJg3RDoba9xbwEmAHrRTOGvgXG+m8t2I8p
     CAC0z2uHF4+eRTC61F89gZk/DLsPy2Hn3wQ9aPJozp6Lp83VZcz07S4lpqb4lfVs
     obgFuQ/OxtyYbDrVKOQabAFW5feshBoInYR8G326YIlvTPkgaxCKAEpUBkuCH+zE
     HT8zo/FntyfvCM8Wbw5L94o6e6SwFFJhglpDfN+v1jZXR14F5kk=
     =CLPC
     -----END PGP SIGNATURE-----
    
    chore: ec2 service + task definition

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,115 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group" "alb" {
+  name = "ecs-sg-alb"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "container" {
+  name = "container-sg"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_container_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.container.id
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_lb_target_group" "ecs" {
+  name     = "ecs-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "ecs" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs.arn
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb" "balancer" {
+  name               = "ecs-elb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
 resource "aws_ecr_repository" "img_repo" {
-  name                 = "personal-img-repo"
+  name                 = "docs"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
@@ -11,27 +118,29 @@ resource "aws_ecr_repository" "img_repo" {
   }
 }
 
-resource "aws_ecr_lifecycle_policy" "img-repo-policy" {
-  repository = aws_ecr_repository.img_repo.name
-
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Expire images older than 3 days",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "sinceImagePushed",
-                "countUnit": "days",
-                "countNumber": 1
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
+resource "aws_ecs_cluster" "sense_eight" {
+  name = "sense-eight"
 }
-EOF
 
+resource "aws_ecs_task_definition" "docs_task" {
+  family                = "docs"
+  container_definitions = file("task-definitions/service.json")
+}
+
+resource "aws_ecs_service" "ecs_docs" {
+  name = "ecs-docs"
+
+  task_definition = aws_ecs_task_definition.docs_task.arn
+  cluster         = aws_ecs_cluster.sense_eight.id
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs.arn
+    container_name   = "cashier-docs"
+    container_port   = 8080
+  }
+
+  desired_count = 1
+}
+output "balancer" {
+  value = aws_lb.balancer
 }
